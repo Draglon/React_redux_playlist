8e79727606f8ae1a0cda81c2067d84e58a6da4ed
Sergey <s.nazarchuck@gmail.com>
2018-08-21 17:19:42 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import './styles/style.less';
+var ReactDOM = require('react-dom');
+var React = require('react');
+var MainComponent = require('./js/Main.js');
+ 
+ReactDOM.render(<MainComponent />, document.getElementById("app"))
---
+++ app/js/Main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class MainComponent extends React.Component {
+    render() {
+        return(
+            <div>
+                asdasdada
+            </div>
+        );
+    }
+}
+
+module.exports = MainComponent;
