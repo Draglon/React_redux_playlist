3d59baa58e6dacd612a3a1e1a18bda5ac86b447d
Sergey <s.nazarchuck@gmail.com>
2018-08-22 13:31:37 +0300

    lesson 6 - combine reducers

--- app/index.js
+++ app/index.js
@@ -1,17 +1,29 @@
-// import './styles/style.less';
-// var ReactDOM = require('react-dom');
-// var React = require('react');
-// var MainComponent = require('./js/Main.js');
- 
-// ReactDOM.render(<MainComponent />, document.getElementById("app"))
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import { createStore } from "redux";
+import App from './js/App';
+import './styles/style.less';
 
-function playlist(state = [], action) {
+const initialState = {
+    tracks: [
+        "Smells like spirit",
+        "Enter Sandman"
+    ],
+    playlists: [
+        'My home playlist',
+        'My work playlist'
+    ]
+}
+
+function playlist(state = initialState, action) {
     if(action.type === 'ADD_TRACK') {
-        return [
-            ...state,
-            action.payload
+        return [...state,
+            {
+                tracks: [...state.tracks, action.payload],
+                playlists: [...state.playlists, action.payload],
+            }
         ];
     }
     return state;
@@ -19,22 +31,9 @@ function playlist(state = [], action) {
 
 const store = createStore(playlist);
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-const trackInput = document.querySelectorAll('.trackInput')[0];
-const list = document.querySelectorAll('.list')[0];
-
-store.subscribe(() => {
-    console.log('subscribe', store.getState());
-    list.innerHTML = '';
-    trackInput.value = '';
-    store.getState().forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    });
-});
-
-addTrackBtn.addEventListener('click', () => {
-    const trackName = trackInput.value;
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
-});
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("app")
+);
---
+++ app/js/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class App extends Component {
+    addTrack() {
+        this.props.onAddTrack(this.trackInput.value);
+        this.trackInput.value = '';
+    }
+
+    render() {
+        console.log(this.props.tracks);
+        console.log(this.props.playlists);
+        return(
+            <div>
+                <div className="form">
+                    <div className="form_field">
+                        <input type="text" className="" ref={(input) => { this.trackInput = input }}/>
+                    </div>
+                    <div className="form_btns">
+                        <button className="" onClick={this.addTrack.bind(this)}>Add Track</button>
+                    </div>
+                </div>
+
+                <ul className="list">
+                    {this.props.tracks.map( (track, index) => 
+                        <li className="" key={index}>{track}</li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    state => ({
+        tracks: state.tracks,
+        playlists: state.playlists
+    }),
+    dispatch => ({
+        onAddTrack: (trackName) => {
+            dispatch({type: 'ADD_TRACK', payload: trackName })
+        }
+    })
+)(App);
